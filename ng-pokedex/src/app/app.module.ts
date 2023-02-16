c7a0596e666d990b996de738b723dcf6d821a8b4
Daniel Fernandez Garcia <daniel.fer.5986@gmail.com>
2023-02-16 12:44:58 -0400

    header html and styling completed :)

--- ng-pokedex/src/app/app.module.ts
+++ ng-pokedex/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SearchSectionComponent } from './search/search-section/search-section.component';
+import { SearchBarComponent } from './search/search-bar/search-bar.component';
 @NgModule({
     declarations: [
         AppComponent,
         PokemonListComponent,
         NavbarComponent,
-        SearchSectionComponent
+        SearchSectionComponent,
+        SearchBarComponent
     ],
     providers: [],
     bootstrap: [AppComponent],
